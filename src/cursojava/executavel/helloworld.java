bfad3610a1db371f579f4e6caa9ad9f217f3041e
Douglas Vilela <douglasvilelaa@gmail.com>
2021-03-01 17:55:51 -0300

    aula variaveis java

--- src/cursojava/executavel/helloworld.java
+++ src/cursojava/executavel/helloworld.java
@@ -3,7 +3,6 @@ package cursojava.executavel;
 public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println("Im Learning Eclipse IDE");
 		
 	}
 }
---
+++ src/cursojava/executavel/variaveis.java
@@ -0,0 +1,20 @@
+package cursojava.executavel;
+
+public class variaveis {
+	public static void main(String[] args) {
+	
+		
+	int maiorIdade = 18;
+	int numero = 10;
+	int idoso = 60;
+	int adolescente = 16;
+	String nome = "Douglas";
+	String cpf = "222.222.222-33";
+	
+	
+	System.out.println("Douglas tem maior idade a partir do " +maiorIdade);
+	System.out.println("Douglas fica idoso a partir do " +idoso);
+	
+	
+	}
+}
